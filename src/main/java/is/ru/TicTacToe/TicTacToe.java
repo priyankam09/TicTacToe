98f0bbbaa2de8151199ef6a5d14eeae6552c984b
Sverrir <sverrirs13@ru.is>
2014-10-26 16:54:16 +0000

    Added Hogriders description to the output.

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -3,7 +3,7 @@ package is.ru.TicTacToe;
 public class TicTacToe{
 
 	public static void main(String[] args){
-		System.out.println("HogRiders!");
+		System.out.println("HogRiders! Crushing skulls is what we do!");
 	}
 
 }
