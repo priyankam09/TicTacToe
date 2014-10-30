4842efc5b2717de2356809028fc241b616585961
Sverrir <sverrirs13@ru.is>
2014-10-30 23:38:36 +0000

    Added constructor to Player class

--- src/main/java/is/ru/TicTacToe/Player.java
+++ src/main/java/is/ru/TicTacToe/Player.java
@@ -1,5 +1,16 @@
 package is.ru.TicTacToe;
 
 public class Player {
+	private String name;
+	private int wins;
+	private char symbol;
+
+	public Player(String newName, char newSymbol) {
+		name = newName;
+		wins = 0;
+		symbol = newSymbol;
+	}
+
+	
 }
 
