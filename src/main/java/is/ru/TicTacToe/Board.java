29116f56ce65df78e29bc30166d60c165d3ba446
Gunnar Orri <gunnark13@ru.is>
2014-10-31 10:19:32 +0000

    Added an empty board constructor and empty functions according to our classDiagram to Board.java

--- src/main/java/is/ru/TicTacToe/Board.java
+++ src/main/java/is/ru/TicTacToe/Board.java
@@ -1,6 +1,36 @@
 package is.ru.TicTacToe;
 
 public class Board {
+    public static final int TILES = 3;
+    private char[][] tiles;
 
+    public Board(){
+        tiles = new char[TILES][TILES];
+        for(int i = 0; i < TILES; i++){
+            for(int j = 0; j < TILES; j++){
+                tiles [i][j] = ' ';
+            }
+        }
+    }
+
+    public void display(){
+        return;
+    }
+
+    public void setTile(int tile, char symbol){
+    	return;
+    }
+
+    public boolean hasWinner(){
+    	return false;
+    }
+
+    public boolean hasRow(char symbol){
+    	return false;
+    }
+
+    public boolean isFull(){
+        return false;
+    }
 }
 
