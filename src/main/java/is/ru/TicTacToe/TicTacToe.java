ce5ac77911e2c756a3435bd2e6bdafaecfb09794
Arnarka13 <arnarka13@ru.is>
2014-10-31 10:44:12 +0000

    added constructor for TicTacToe

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -2,8 +2,18 @@ package is.ru.TicTacToe;
 
 public class TicTacToe{
 
+	private Player player1;
+	private Player player2;
+	private Board board;
+
+	public TicTacToe(String name1, String name2) {
+		player1 = new Player(name1, 'X');
+		player2 = new Player(name2, 'X');
+		board = new Board();
+	}
+
 	public static void main(String[] args){
-		System.out.println("HogRiders! Crushing skulls is what we do!");
+		
 	}
 
 }
