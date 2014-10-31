fb4ecd7a9aaaa64c08a5b36c5d640e88054611f4
Gunnar Orri <gunnark13@ru.is>
2014-10-31 11:17:09 +0000

    Wrote one test for setTile

--- src/test/java/is/ru/TicTacToeTest/BoardTest.java
+++ src/test/java/is/ru/TicTacToeTest/BoardTest.java
@@ -1,1 +1,17 @@
 /*Test class for board */
+package is.ru.TicTacToe;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BoardTest {
+       public static void main(String args[]) {
+       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.BoardTest");
+    }
+
+    @Test
+    public void testSetTile(){
+       Board board = new Board();
+               board.setTile(1, 'X');
+               assertEquals('X', board.getTile(0, 0));
+    }
+}
