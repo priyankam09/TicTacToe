a99749e0a2c5c37fe0997e3f32352088f95e0f03
Gunnar Orri <gunnark13@ru.is>
2014-10-31 14:24:36 +0000

    Removed all, starting from scratch

--- src/main/java/is/ru/TicTacToe/Board.java
+++ src/main/java/is/ru/TicTacToe/Board.java
@@ -13,13 +13,7 @@ public class Board {
         }
     }
     public char getTile(int tile) {
-
-    	if(tile > 0 && tile < 10){
-			int i = (tile + 2) % 3;
-			int j = (tile - 1) / 3;
-			return tiles[i][j];
-		}else
-			throw new IndexOutOfBoundsException("Tile out of bounds");
+    	return '0';
     }
 
     public void display(){
@@ -31,17 +25,7 @@ public class Board {
     }
 
     public void setTile(int tile, char symbol){
-		
-		if(tile > 0 && tile < 10){
-			int i = (tile + 2) % 3;
-			int j = (tile - 1) / 3;
-			if(getTile(tile) == ' '){
-				tiles[i][j] = symbol;
-			}else
-				throw new TileAlreadySetException("Tile already set");
-		}else
-			throw new IndexOutOfBoundsException("Tile out of bounds");
-
+		return;
     }
 
     public boolean hasWinner(){
--- src/test/java/is/ru/TicTacToeTest/BoardTest.java
+++ src/test/java/is/ru/TicTacToeTest/BoardTest.java
@@ -10,41 +10,4 @@ public class BoardTest {
        public static void main(String args[]) {
        org.junit.runner.JUnitCore.main("is.ru.stringcalculator.BoardTest");
     }
-
-    @Test
-    public void testSetTile(){
-       Board board = new Board();
-               board.setTile(1, 'X');
-               assertEquals('X', board.getTile(1));
-    }
-    @Rule
-    public ExpectedException ex = ExpectedException.none();
-
-    @Test
-    public void testGetTileOutOfBounds(){
-    
-    	ex.expect(IndexOutOfBoundsException.class);
-    	ex.expectMessage(equalTo("Tile out of bounds"));
-
-    	Board board = new Board();
-        board.getTile(-1);
-    }
-
-        @Test
-    public void testSetTileOutOfBounds(){
-    
-    	ex.expect(IndexOutOfBoundsException.class);
-    	ex.expectMessage(equalTo("Tile out of bounds"));
-
-    	Board board = new Board();
-        board.setTile(-1, 'X');
-    }
-
-    @Test
-    public void testSetTileAlreadySet(){
-
-    	Board board = new Board();
-    	board.setTile(1, 'X');
-        assertTrue(!board.setTile(1, 'X'));
-    }
 }
