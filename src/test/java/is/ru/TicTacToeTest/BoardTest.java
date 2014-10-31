ec6309494bb4dd941c90f94669871bed4dd1760a
Gunnar Orri <gunnark13@ru.is>
2014-10-31 14:29:42 +0000

    added testSetTile

--- src/test/java/is/ru/TicTacToeTest/BoardTest.java
+++ src/test/java/is/ru/TicTacToeTest/BoardTest.java
@@ -10,4 +10,11 @@ public class BoardTest {
        public static void main(String args[]) {
        org.junit.runner.JUnitCore.main("is.ru.stringcalculator.BoardTest");
     }
+
+    @Test
+    public void testSetTile(){
+    	Board board = new Board();
+    	board.setTile(1, 'X');
+    	assertEquals('X', board.getTile(1));
+    }
 }
