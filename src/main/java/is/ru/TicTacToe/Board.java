5a46fa034e9c3567adce0d00873096a9b04dbfff
Gunnar Orri <gunnark13@ru.is>
2014-10-31 11:27:02 +0000

    Implemented setTile

--- src/main/java/is/ru/TicTacToe/Board.java
+++ src/main/java/is/ru/TicTacToe/Board.java
@@ -21,7 +21,33 @@ public class Board {
     }
 
     public void setTile(int tile, char symbol){
-    	return;
+		int i, j;
+		switch(tile) {
+		case 1:
+		case 2:
+		case 3:
+			i = 0;
+			j = tile - 1;
+			break;
+		case 4:
+		case 5:
+		case 6:
+			i = 1;
+			j = tile - 4;
+			break;
+		case 7:
+		case 8:
+		case 9:
+			i = 2;
+			j = tile - 7;
+			break;
+		default:
+			i = -1;
+			j = -1;
+		}
+		if(!(i == -1 && j == -1)) {
+			tiles[i][j] = symbol;
+		}
     }
 
     public boolean hasWinner(){
