3582a560725a910d16e3e9843cc8c03cde002b0b
Arnarka13 <arnarka13@ru.is>
2014-11-01 11:01:35 +0000

    added unit test for switchPlayerTurn

--- src/test/java/is/ru/TicTacToeTest/TicTacToeTest.java
+++ src/test/java/is/ru/TicTacToeTest/TicTacToeTest.java
@@ -1,6 +1,17 @@
 package is.ru.TicTacToe;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TicTacToeTest {
 
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("TicTacToe.TicTacToeTest");
+	}
+
+	@Test
+	public void testSwitchPlayerTurn(){
+		TicTacToe game = new TicTacToe();
+		game.switchPlayerTurn();
+		assertEquals("player2", game.getPlayerTurn().getName());
+	}
 }
